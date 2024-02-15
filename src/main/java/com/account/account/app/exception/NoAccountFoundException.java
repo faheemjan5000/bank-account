da3af01dbf773053dcfa10fd1a854c11b667173e
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2024-02-15 15:01:01 +0100

    new exception added

---
+++ src/main/java/com/account/account/app/exception/NoAccountFoundException.java
@@ -0,0 +1,7 @@
+package com.account.account.app.exception;
+
+public class NoAccountFoundException extends Exception{
+    public NoAccountFoundException(String message) {
+        super(message);
+    }
+}
