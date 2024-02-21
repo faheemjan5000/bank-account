5efeee267234d598de3fb15f334b811f410c08d0
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2024-02-21 16:44:44 +0100

    swagger added

---
+++ src/main/java/com/account/account/app/config/SwaggerConfig.java
@@ -0,0 +1,61 @@
+package com.account.account.app.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableSwagger2
+@ConfigurationProperties("app.api")
+@ConditionalOnProperty(name="app.api.swagger.enable", havingValue = "true", matchIfMissing = false)
+public class SwaggerConfig {
+
+    private String version;
+    private final String title = "TITLE OF THE SERVICE";
+    private final String description="description of the service";
+    private final String basePackage = "com.account.account.app.config";
+    private final String contactName = "contact name";
+    private String contactEmail;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .paths(PathSelectors.any())
+                .build()
+                .directModelSubstitute(LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(LocalDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .version(version)
+                .contact(new Contact(contactName, null, contactEmail))
+                .build();
+    }
+
+    /**
+     ...
+     ...
+     Getters
+     Setters
+     ...
+     ...
+     **/
+}
